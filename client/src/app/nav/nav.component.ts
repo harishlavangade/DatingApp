dbd2e6767b2ae6f7fd03ba41feb437890cb40d40
harishlavangade <harish.lavangade@gmail.com>
2021-07-20 16:56:49 +0400

    create services

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,38 @@
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+ model: any = {}
+ loggedIn: boolean =false;
+
+  constructor(private accountService:AccountService ) { }
+
+  ngOnInit(): void {
+  }
+
+  login()
+  {
+    this.accountService.login(this.model).subscribe(response => 
+      {
+        console.log(response);
+        this.loggedIn=true;
+      },error => 
+      {
+        console.log(error);
+      }
+      );
+    console.log(this.model);
+  }
+
+  logout()
+  {
+    this.loggedIn=false;
+  }
+
+}
