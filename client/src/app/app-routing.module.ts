14c677a095a330042cdc80e2953f51cab1045b19
harishlavangade <harish.lavangade@gmail.com>
2021-07-21 02:05:20 +0400

    Routing concept

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'members',component:MemberListComponent},
+  {path:'members/:id',component:MemberDetailComponent},
+  {path:'lists',component:ListsComponent},
+  {path:'Messages',component:Message},
+  {path:'**',component:HomeComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
