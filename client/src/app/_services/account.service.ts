d774d6f0d1dde5830521d24b0233472a29cf0b0b
harishlavangade <harish.lavangade@gmail.com>
2021-07-21 01:48:22 +0400

    section 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import {map} from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +11,58 @@ import { Injectable } from '@angular/core';
 export class AccountService {
 
   baseUrl='https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http:HttpClient) { }
 
   login(model:any)
   {
-    return this.http.post(this.baseUrl+'account/login',model);
+    //return this.http.post(this.baseUrl+'account/login',model);
+    return this.http.post(this.baseUrl+'account/login',model).pipe(
+      map(( response: any ) => {
+        const user = response.token;
+        console.log(user);
+        if(user)
+        {
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }        
+      })
+    )
+  }
+register(model:any){
+return this.http.post(this.baseUrl +'account/register',model).pipe(
+  map((user: any) => {
+    if(user)
+    {
+      localStorage.setItem('user',JSON.stringify(user));
+      this.currentUserSource.next(user);
+    }
+    return user;
+  })
+)
+}
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  // below is simple map function
+  // login(model:any)
+  // {
+  //   //return this.http.post(this.baseUrl+'account/login',model);
+  //   return this.http.post(this.baseUrl+'account/login',model).pipe(
+  //     map((response: any) =>{
+  //       const user = response;
+  //       if(user)
+  //       {
+  //         localStorage.setItem('user',JSON.stringify(user));
+  //       }
+  //     } )
+  //   )
+  // }
+  logout(){
+    localStorage.removeItem('user');
+    //this.currentUserSource.next(null);
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,24 +12,26 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
  model: any = {}
  loggedIn: boolean =false;
+ currentUser$!: Observable<User>;
 
   constructor(private accountService:AccountService ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   login()
   {
-    this.accountService.login(this.model).subscribe(response => 
-      {
-        console.log(response);
-        this.loggedIn=true;
-      },error => 
-      {
-        console.log(error);
-      }
-      );
+    this.accountService.login(this.model);
     console.log(this.model);
+  }
+
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    },error =>
+    {
+      console.log(error);
+    })
   }
 
   logout()
