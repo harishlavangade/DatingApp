c2acdbf2b0a9fe66a298b2c908e5df5335e82078
harishlavangade <harish.lavangade@gmail.com>
2021-08-07 17:44:42 +0400

    Photo set main and delete

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -60,5 +60,9 @@ members:Member[] =[];
     return this.http.put(this.baseUrl+'users/set-main-photo/' + photoId,{});
   }
 
+  deletePhoto(photoId:number){
+    return this.http.delete(this.baseUrl +'users/delete-photo/' +photoId);
+  }
+
 }
 
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -45,6 +45,13 @@ user?:User;
       })
     })
   }
+
+  deletePhoto(photoId:number){
+    this.memberService.deletePhoto(photoId).subscribe(()=>{
+      this.member!.photos = this.member!.photos.filter(x=>x.id != photoId);
+    })
+  }
+
   uploadAll(){
 
   }
